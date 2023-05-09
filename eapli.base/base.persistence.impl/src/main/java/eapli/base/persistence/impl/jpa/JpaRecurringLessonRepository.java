35b77528446852f6661fcad1cd3f865a08f98b57
miguelseixas <1100358@isep.ipp.pt>
2023-05-09 15:15:02 +0100

    [Sprint B][US1010] Add JPA Rec Lesson Repo
    Add the Recurring Lesson JPA Repository

---
+++ eapli.base/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaRecurringLessonRepository.java
@@ -0,0 +1,7 @@
+package eapli.base.persistence.impl.jpa;
+
+import eapli.framework.infrastructure.repositories.impl.jpa.JpaAutoTxRepository;
+import eapli.base.*
+
+public class JpaRecurringLessonRepository extends JpaAutoTxRepository<> implements RecurringLessonRepository {
+}
