95bf8de4f9166a8fad4308a51ab92881f8e79341
miguelseixas <1100358@isep.ipp.pt>
2023-05-10 00:45:22 +0100

    [Sprint B][US1010] Update Rec Lesson Controller
    Updated the use of spring framework on the controller

--- eapli.base/Lesson.application/src/main/java/eapli/base/lesson/application/CreateRecurringLessonController.java
+++ eapli.base/Lesson.application/src/main/java/eapli/base/lesson/application/CreateRecurringLessonController.java
@@ -6,6 +6,10 @@ import eapli.base.lesson.domain.repositories.RecurringLessonRepository;
 import eapli.framework.application.UseCaseController;
 import eapli.framework.general.domain.model.Designation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.time.Duration;
 import java.util.Calendar;
 
@@ -13,22 +17,19 @@ import java.util.Calendar;
  *  The controller for the use case "Schedule a Class" using the domain objects.
  *
  */
+@Component
 @UseCaseController
 public class CreateRecurringLessonController {
 
+    @Autowired
     private final RecurringLessonRepository recurringLessonRepository;
 
     public CreateRecurringLessonController(final RecurringLessonRepository recurringLessonRepository) {
         this.recurringLessonRepository = recurringLessonRepository;
     }
 
-    /*public RecurringLesson createRecurringLesson(final Designation title, final Calendar startDate, final Calendar endDate, final Duration duration) {
-        return RecurringLesson(title, startDate, endDate, duration);
-    }
-
-     */
-
-    public RecurringLesson ccreateRecurringLesson(final Designation title, final Calendar startDate, final Calendar endDate, final Duration duration) {
+    @Transactional
+    public RecurringLesson createRecurringLesson(final Designation title, final Calendar startDate, final Calendar endDate, final Duration duration) {
 
         final var newRecurringLesson = new RecurringLessonBuilder().titled(title)
                 .starting(startDate).ending(endDate)
