96a497291b1dd2463e58cc7b400edb0647b4b42c
MiguelCCardoso <Miguel.ccardoso92@gmail.com>
2023-05-19 23:20:17 +0100

    [SPRINTB][US1012] Minor adjustment in the way the minutes were entered when creating a recurring lesson and in the way the updating was done

--- eCourse.app.teacher.console/src/main/java/eCourse/app/teacher/console/presentation/RecurringLessons/UpdateScheduleRecurringLessonUI.java
+++ eCourse.app.teacher.console/src/main/java/eCourse/app/teacher/console/presentation/RecurringLessons/UpdateScheduleRecurringLessonUI.java
@@ -24,7 +24,7 @@ public class UpdateScheduleRecurringLessonUI extends AbstractUI {
 
         int position = 0;
         for (RecurringLesson lesson: listOfLessons) {
-            System.out.printf("(%d) %s %s %s %s",position, lesson.title(), lesson.startDate().getTime(), lesson.endDate().getTime(), lesson.duration().toString());
+            System.out.printf("(%d) %s %s %s %s",position, lesson.title(), lesson.startDate().getTime(), lesson.endDate().getTime(), lesson.duration());
             lessonList.add(lesson);
             position ++;
         }
@@ -33,7 +33,7 @@ public class UpdateScheduleRecurringLessonUI extends AbstractUI {
 
         Calendar startDate = Console.readCalendar("The old start date is " + lessonList.get(chooseLesson).startDate().getTime() + " what value you want to enter?","dd/mm/yyyy hh:mm");
         Calendar endDate = Console.readCalendar("The old end date is " + lessonList.get(chooseLesson).endDate().getTime() + " what value you want to enter?","dd/mm/yyyy");
-        Duration duration = Duration.ofMinutes(Console.readInteger("The old duration is " + lessonList.get(chooseLesson).startDate().toString() + " what value you want to enter?"));
+        int duration = Console.readInteger("The old duration is " + lessonList.get(chooseLesson).startDate().toString() + " what value you want to enter?");
 
         theController.updateScheduleOfRecurringLesson(lessonList.get(chooseLesson),startDate,endDate,duration);
 
