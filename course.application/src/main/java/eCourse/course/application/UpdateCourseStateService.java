9d8c8fdfff33334c1c1c6f30e03443bf82647441
DuarteFonseca <1210850@isep.ipp.pt>
2023-05-21 12:52:21 +0100

    [Sprint B][US1004] - UI and service changes to restrict selection of course states

--- course.application/src/main/java/eCourse/course/application/UpdateCourseStateService.java
+++ course.application/src/main/java/eCourse/course/application/UpdateCourseStateService.java
@@ -20,7 +20,7 @@ public class UpdateCourseStateService {
 
     public void open(String designationName) {
         //1 - validate if the user is authenticated and has a valid role
-        authorizationService.ensureAuthenticatedUserHasAnyOf(ECourseRoles.MANAGER);
+        //authorizationService.ensureAuthenticatedUserHasAnyOf(ECourseRoles.MANAGER);
         authorizationService.ensureAuthenticatedUserHasAnyOf(ECourseRoles.ADMIN);
 
         //2 - validate params
@@ -87,7 +87,7 @@ public class UpdateCourseStateService {
 
     public void close(String designationName) {
         //1 - validate if the user is authenticated and has a valid role
-        authorizationService.ensureAuthenticatedUserHasAnyOf(ECourseRoles.MANAGER);
+        //authorizationService.ensureAuthenticatedUserHasAnyOf(ECourseRoles.MANAGER);
         authorizationService.ensureAuthenticatedUserHasAnyOf(ECourseRoles.ADMIN);
 
         //2 - validate params
@@ -98,7 +98,7 @@ public class UpdateCourseStateService {
                 .orElseThrow(() -> new NoSuchElementException("The course " + designationName + " does not exist in the database"));
 
         //the course can only transition to CLOSE if it comes from the PROGRESS state
-        if (!String.valueOf(course.state()).equals(String.valueOf(BaseCourseStates.PROGRESS).toUpperCase())){
+        if (!String.valueOf(course.state()).equals(String.valueOf(BaseCourseStates.PROGRESS))){
             throw new IllegalArgumentException("The course " + designationName + " has the state " + course.state()
                     + " which cannot transition to " + BaseCourseStates.CLOSE);
         }
