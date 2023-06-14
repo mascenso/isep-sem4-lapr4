562f607ce71bf974e5aa3c96f0b75a3f411820b9
miguelseixas <1100358@isep.ipp.pt>
2023-06-14 21:47:37 +0100

    [Sprint B][US1008] Start Testing

---
+++ Enrollment.domain/src/test/java/eCourse/domain/CourseEnrollmentRequestTest.java
@@ -0,0 +1,13 @@
+package eCourse.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseEnrollmentRequestTest {
+
+    private Course course1;
+    private Course course2;
+    private Student student1;
+    private Student student2;
+
+
+}
