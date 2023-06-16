41f6e6f12f24953c32ed39ab105ede21c87608d5
mascenso <1210855@isep.ipp.pt>
2023-06-16 18:42:58 +0100

    [Sprint C] [US1011] Add UI for creating an extra lesson

--- course.domain/src/main/java/eCourse/domain/Course.java
+++ course.domain/src/main/java/eCourse/domain/Course.java
@@ -43,13 +43,6 @@ public class Course implements AggregateRoot<Designation> {
     @ElementCollection(fetch = FetchType.EAGER)
     private final Set<StudentsInCourse> students = new HashSet<>();
 
-    /*@ManyToMany(cascade = {CascadeType.ALL})
-    @JoinTable(
-            name = "Course_Student",
-            joinColumns = {@JoinColumn(name = "IDCOURSE")},
-            inverseJoinColumns = {@JoinColumn(name = "NUMBER")}
-    )
-    Set<Student> students = new HashSet<>();*/
     protected Course (final Designation name, final Description description, final CourseEdition edition, SystemUser teacherCordinator){
         Preconditions.noneNull(name,description,edition);
 
